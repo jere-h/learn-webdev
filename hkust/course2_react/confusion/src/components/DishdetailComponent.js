bf26ed5411a81eabaea675bafaae7d54a14604b5
Jeremy H <jeremyhyf@gmail.com>
2021-01-07 13:09:30 +0800

    Redux Actions (new actiontype link to actioncreators & commentreducer, new actioncreator link to main with mapdispatch, new case in commentreducer to return concat state)

--- hkust/course2_react/confusion/src/components/DishdetailComponent.js
+++ hkust/course2_react/confusion/src/components/DishdetailComponent.js
@@ -26,13 +26,12 @@ class CommentForm extends Component {
 
 	handleSubmit(values) {
 		this.toggleModal();
-		console.log('Current State is: ' + JSON.stringify(values));
-        alert('Current State is: ' + JSON.stringify(values));
+		this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
 	}
 
 	render() {
 		return(
-			<React.Fragment>
+			<div>
 				<Button outline onClick={this.toggleModal}>
 					<span className="fa fa-pencil fa-lg"></span> Submit Comment
 				</Button>
@@ -55,9 +54,9 @@ class CommentForm extends Component {
 								</Col>
 							</Row>
 							<Row className="form-group">
-								<Label htmlFor="name" className="col-12">Your Name</Label>
+								<Label htmlFor="author" className="col-12">Your Name</Label>
 								<Col xs={12}>
-								<Control.text model=".name" id="name" name="name"
+								<Control.text model=".author" id="author" name="author"
 									placeholder="Your Name"
 									className="form-control"
 									validators={{
@@ -84,7 +83,7 @@ class CommentForm extends Component {
 						</LocalForm>
 					</ModalBody>
 				</Modal>
-			</React.Fragment>
+			</div>
 		)
 	}
 }
@@ -103,7 +102,7 @@ function RenderDish({dish}) {  // same function as gone through in the videos
 	);
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, dishId}) {
 	if(comments != null) {
 		return (
 			<div className="col-12 col-md-5 m-1">
@@ -120,7 +119,7 @@ function RenderComments({comments}) {
 					 })
 					}
 				</ul>
-				<CommentForm />
+				<CommentForm dishId={dishId} addComment={addComment}/>
 			</div>
 		);
 	}
@@ -147,7 +146,9 @@ const DishDetail = (props) => {
 				</div>
 				<div className="row">
 					<RenderDish dish={props.dish} />
-					<RenderComments comments={props.comments} />
+					<RenderComments comments={props.comments}
+						addComment={props.addComment}
+						dishId={props.dish.id} />
 				</div>
 			</div>
 		);
