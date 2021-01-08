3da76c0be9e5780513952ba820bd204c6e26a525
Jeremy H <jeremyhyf@gmail.com>
2021-01-08 13:32:00 +0800

    Redux Thunk LoadingComponent

--- hkust/course2_react/confusion/src/components/DishdetailComponent.js
+++ hkust/course2_react/confusion/src/components/DishdetailComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Label, Col, Row, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { LocalForm, Control, Errors } from 'react-redux-form'
+import { LocalForm, Control, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
@@ -131,7 +132,23 @@ function RenderComments({comments, addComment, dishId}) {
 }
 
 const DishDetail = (props) => {
-	if (props.dish != null) {
+	if (props.isLoading) {
+		return(
+			<div className="container">
+				<div className="row">
+					<Loading />
+				</div>
+			</div>
+		);
+	}
+	else if (props.errMess) {
+		<div className="container">
+			<div className="row">
+				<h4>{props.errMess}</h4>
+			</div>
+		</div>
+	}
+	else if (props.dish != null) {
 		return (
 			<div className="container">
 				<div className="row">
